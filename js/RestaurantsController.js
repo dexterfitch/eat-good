f7aa133854620b710c1d43f6496fccb63bcfef8a
Dex Fitch <fitch.dexter@gmail.com>
2015-02-23 13:56:42 -0800

    Add ability to add restaurants, and display them with web-address hyperlinked, and spendy-level printed.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,4 +1,24 @@
 restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
-  $scope.restaurants = [{ name: "Jombo's Zombie Shrimp" }];
+  $scope.restaurants = [];
+
+  $scope.addRestaurant = function() {
+    var spendyLevelToText = "";
+    if ($scope.restaurantSpendyLevel === "1") {
+      spendyLevelToText = "Under $8"
+    } else if ($scope.restaurantSpendyLevel === "2") {
+      spendyLevelToText = "$8 - $15"
+    } else if ($scope.restaurantSpendyLevel === "3") {
+      spendyLevelToText = "$16 - $25"
+    } else if ($scope.restaurantSpendyLevel === "4") {
+      spendyLevelToText = "$26 - $35"
+    } else {
+      spendyLevelToText = "over $35"
+    }
+
+    $scope.restaurants.push({ name: $scope.restaurantName, website: $scope.restaurantWebsite, spendyText: spendyLevelToText, spendyLevel: $scope.restaurantSpendyLevel });
+    $scope.restaurantName = null;
+    $scope.restaurantWebsite = null;
+    $scope.restaurantSpendyLevel = "";
+  };
 
 });
