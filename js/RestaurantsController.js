d9b26a8ddef52120d6c1344bbe1cde76489ec6ae
Dex Fitch <fitch.dexter@gmail.com>
2015-02-23 15:37:14 -0800

    Add edit function, adjust page styling/display.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,26 +1,30 @@
 restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
 
-  $scope.addRestaurant = function() {
+  $scope.createSpendyText = function(spendyLevel) {
     var spendyLevelToText = "";
 
-    if ($scope.restaurantSpendyLevel === "1") {
+    if (spendyLevel === "1") {
       spendyLevelToText = "Under $8"
-    } else if ($scope.restaurantSpendyLevel === "2") {
+    } else if (spendyLevel === "2") {
       spendyLevelToText = "$8 - $15"
-    } else if ($scope.restaurantSpendyLevel === "3") {
+    } else if (spendyLevel === "3") {
       spendyLevelToText = "$16 - $25"
-    } else if ($scope.restaurantSpendyLevel === "4") {
+    } else if (spendyLevel === "4") {
       spendyLevelToText = "$26 - $35"
     } else {
       spendyLevelToText = "Over $35"
     }
 
+    return spendyLevelToText;
+  }
+
+  $scope.addRestaurant = function() {
+
     $scope.restaurants.push({
       name: $scope.restaurantName,
       website: $scope.restaurantWebsite,
       cuisine: $scope.restaurantCuisine,
-      spendyText: spendyLevelToText,
       spendyLevel: $scope.restaurantSpendyLevel
     });
 
